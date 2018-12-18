a3d7725fe860f4e06688bb34caa9d94590a013af
mvo12341@gmail.com <mvo12341@gmail.com>
2018-12-18 15:59:08 +0800

    done linear regression

--- LinearReg/linear regression_skLearn.py
+++ LinearReg/linear regression_skLearn.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import scipy.stats as stats
 from sklearn.linear_model import LinearRegression
+from sklearn import cross_validation
 from sklearn.datasets import load_boston
 
 # Load the diabetes dataset
@@ -55,4 +56,24 @@ print(mean_sq_err)
 
 ## ---------------- ##
 # percentage split
-X_train, X_test, Y_train, Y_test = sklearn.cross_validation.train
+train_feature, test_feature, train_price, test_price = cross_validation.train_test_split(training_data, data_frame['PRICE'], test_size=0.33, random_state=5)
+linear_mod = LinearRegression()
+linear_mod.fit(train_feature, train_price)
+pred_train = linear_mod.predict(train_feature)
+pred_test = linear_mod.predict(test_feature)
+print('--------------------')
+print('--------------------')
+print('\nTrain_feature\n')
+print(train_feature.head())
+print('\nTest_feature\n')
+print(test_feature.head())
+print('\nTrain_price\n')
+print(train_price.head())
+print('\nTest_price\n')
+print(test_price.head())
+'''
+mean_sq_err = np.mean((train_price - linear_mod.predict(train_feature)) ** 2)
+print('fit model train_feature and MSE with test_feature', mean_sq_err)
+mean_sq_err = np.mean((test_price - linear_mod.predict(test_feature)) ** 2)
+print('fit model train_feature and MSE with test_feature, test_price', mean_sq_err)
+'''
